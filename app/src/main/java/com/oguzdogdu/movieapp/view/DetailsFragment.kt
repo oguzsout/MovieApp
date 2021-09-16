de29fd4a0c3d22d5ba4ecb7a867d0be68f9bf333
Oguzhan Dogdu <oguzd494@gmail.com>
2021-09-16 21:41:08 +0300

    Dagger-Hilt Setup

--- app/src/main/java/com/oguzdogdu/movieapp/view/DetailsFragment.kt
+++ app/src/main/java/com/oguzdogdu/movieapp/view/DetailsFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.oguzdogdu.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsFragment : Fragment() {
 
 
